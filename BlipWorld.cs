9081fcf4cfb1b9575b51da946460ee3a14a4f628
Haze <115841732+HazeStudioFR@users.noreply.github.com>
2025-11-21 17:18:56 -0500

    Added console command "CleanupBlipBloats".

---
+++ BlipWorld.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Rage;
+using Rage.Native;
+
+namespace NoBlipBloat;
+
+internal static class BlipWorld
+{
+    internal static IEnumerable<Blip> GetAllBlips()
+    {
+        foreach (BlipSprite sprite in Enum.GetValues(typeof(BlipSprite)))
+        {
+            int handle = NativeFunction.Natives.GET_FIRST_BLIP_INFO_ID<int>((int)sprite);
+            while (NativeFunction.Natives.DOES_BLIP_EXIST<bool>(handle))
+            {
+                var blip = World.GetBlipByHandle((uint)handle);
+                if (blip != null) yield return blip;
+                handle = NativeFunction.Natives.GET_NEXT_BLIP_INFO_ID<int>((int)sprite);
+            }
+        }
+    }
+}
---
+++ ConsoleCommands.cs
@@ -0,0 +1,22 @@
+﻿using Rage;
+using Rage.Attributes;
+
+namespace NoBlipBloat;
+
+public static class ConsoleCommands
+{
+    [ConsoleCommand(Description = "Removes blips created by other plugins.", Name = "CleanupBlipBloats")]
+    public static void CleanupBlips([ConsoleCommandParameter(Name = "Include LSPDFR Station Blips", Description = "if true, will delete lspdfr station blips from map. Jail not included.")] bool removeLSPDFRBlips)
+    {
+        foreach (var blip in BlipWorld.GetAllBlips())
+        {
+            if (!blip.Exists())
+                return;
+
+            if (!removeLSPDFRBlips && blip.Sprite == BlipSprite.PoliceStation)
+                return;
+
+            blip.Delete();
+        }
+    }
+}
